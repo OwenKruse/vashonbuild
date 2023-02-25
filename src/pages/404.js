d42bdb4b6fb63535c7f4ddee4e6728643bccdfd6
owenkruse <kruse.owen@vashonsd.org>
2023-02-24 17:55:16 -0800

    Deployment

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,18 @@
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 
 export default function Custom404() {
     const router = useRouter()
+
+    useEffect(() => {
+            router.push('/')
+    }, [router])
     // Push the user to the home page
-    router.push('/')
-    return <h1>404 - Page Not Found</h1>
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+        </div>
+    )
+
 }
